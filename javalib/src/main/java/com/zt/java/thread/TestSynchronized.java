fd5f7a4e571b5814d4d87f02190e222e47902a17
totomiz <461609086@qq.com>
2017-10-03 16:54:18 +0800

    add:test synchronized

---
+++ javalib/src/main/java/com/zt/java/thread/TestSynchronized.java
@@ -0,0 +1,44 @@
+package com.zt.java.thread;
+
+/**
+ * Created by tony.zhang
+ * Email: tony.zhang.Mr@foxmail.com
+ */
+
+public class TestSynchronized extends Thread{
+    public  static int count=0;
+
+    public synchronized void funtion(){
+        //保护函数
+    }
+
+    public void code(){
+        synchronized (this){
+            //保护代码块
+        }
+    }
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            for (int i = 0; i < 10; i++) {
+                count++;
+                System.out.println(currentThread().getName() + " for loop " + count);
+            }
+        }
+//        synchronized (this){
+//            for (int i = 0; i < 10; i++) {
+//                System.out.println(currentThread().getName()+" synchronized loop "+i);
+//            }
+//        }
+    }
+
+    public static void main(String[] args){
+        TestSynchronized t1=new TestSynchronized();
+        TestSynchronized t2=new TestSynchronized();
+        Thread thread1=new Thread(t1,"test1");
+        Thread thread2=new Thread(t2,"test2");
+        t1.start();
+        t2.start();
+    }
+}
